6bbee2e7cab255efd7cb4e86fcf5713e9acd7c71
Konstantin Mikheev <mik.kon6610@gmail.com>
2024-02-16 08:38:07 +0100

    try hide it

--- src/variant-4/BoxesOfTV.tsx
+++ src/variant-4/BoxesOfTV.tsx
@@ -3,20 +3,18 @@ import { BoxTV } from './BoxTV';
 
 export const BoxesOfTV = () => {
   return (
-    <>
-      <div className="p-4 flex flex-col gap-4 cursor-pointer mb-[164px]">
-        {data.slice(0, 4).map(item => (
-          <BoxTV
-            category={item.category}
-            percentage={item.percentage}
-            subcategory={item.subcategory}
-            title={item.title}
-            key={item.title}
-            img={item.logo}
-            bigImg={item.imgs[0]}
-          />
-        ))}
-      </div>
-    </>
+    <div className="p-4 flex flex-col gap-4 cursor-pointer mb-[164px]">
+      {data.slice(0, 4).map(item => (
+        <BoxTV
+          category={item.category}
+          percentage={item.percentage}
+          subcategory={item.subcategory}
+          title={item.title}
+          key={item.title}
+          img={item.logo}
+          bigImg={item.imgs[0]}
+        />
+      ))}
+    </div>
   );
 };
